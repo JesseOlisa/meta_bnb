6a8ea03b342683b0d96ab5417b00e70466920524
JESSE OGBONNA <jessesamuel84@gmail.com>
2022-11-11 16:13:56 +0200

    adds images to codebase

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <img src="/images/image_66.svg" alt="" />
+    </div>
+    
   )
 }
 
